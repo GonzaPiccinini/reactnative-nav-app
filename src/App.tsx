dba7995204f3eec180aa938c6f9a15379afdfc94
Gonzalo Piccinini <gonzipiccinini@gmail.com>
2024-05-04 05:46:33 -0300

    Stack.Navigation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
+import 'react-native-gesture-handler'
+
 import { NavigationContainer } from '@react-navigation/native'
-import React from 'react'
-import { Text, View } from 'react-native'
+import { StackNavigator } from './presentation/routes/StackNavigator'
 
 const App = () => {
   return (
     <NavigationContainer>
-      <View>
-        <Text>App.tsx</Text>
-      </View>
+      <StackNavigator>
+
+      </StackNavigator>
     </NavigationContainer>
   )
 }
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Pressable, Text, View } from 'react-native'
+import { globalStyles } from '../../theme/theme'
+import { useNavigation } from '@react-navigation/native'
+import { PrimaryButton } from '../../components/shared/PrimaryButton'
 
 const HomeScreen = () => {
+  const navigation = useNavigation()
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={globalStyles.container}>
+      <PrimaryButton 
+      label='Productos' 
+      onPress={() => navigation.navigate('Products' as never)}
+      />
+      <PrimaryButton 
+      label='Configuración' 
+      onPress={() => navigation.navigate('Settings' as never)}
+      />
     </View>
   )
 }
